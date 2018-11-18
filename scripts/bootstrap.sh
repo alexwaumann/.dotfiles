a2b9dc205647458b613e0ebc7e2bf145dcb962b7
Alejandro Waumann <awaumann@gmail.com>
2018-11-18 17:04:53 -0600

    fixed broken paths

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,7 @@
-#!/user/bin/zsh
+#!/usr/bin/zsh
 
 # setup plugin managers
-sh nvim/setup_vimplug.sh
-sh zsh/setup_zplug.sh
+sh ../nvim/setup_vimplug.sh
+sh ../zsh/setup_zplug.sh
 
 # need to setup symlinks as well
